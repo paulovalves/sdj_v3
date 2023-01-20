24a2178f27ff922e4e2d434348c40968949b3e26
paulovalves <paulovalves@outlook.com>
2023-01-19 22:14:10 -0300

    indentação

--- components/forms/PesquisarEnteForm.vue
+++ components/forms/PesquisarEnteForm.vue
@@ -1,41 +1,41 @@
 <template>
     <v-row justify="space-between" >
+      <v-col cols='12' sm="4">
+        <v-form ref="form">
+          <v-text-field
+            v-model="model"
+            :counter="max"
+            :rules="rules"
+            label="Nome do Ente:"
+          ></v-text-field>
+        </v-form>
+      </v-col>
+      
+      <v-col cols='12' sm="4">
+        <v-form ref="form">
+          <v-text-field
+            v-model="model"
+            :counter="max"
+            :rules="rules"
+            label="CNPJ do Ente:"
+          ></v-text-field>
+        </v-form>
+      </v-col>
 
-          <v-col cols='12' sm="4">
-            <v-form ref="form">
-              <v-text-field
-                v-model="model"
-                :counter="max"
-                :rules="rules"
-                label="Nome do Ente:"
-              ></v-text-field>
-            </v-form>
-          </v-col>
-          <v-col cols='12' sm="4">
-            <v-form ref="form">
-              <v-text-field
-                v-model="model"
-                :counter="max"
-                :rules="rules"
-                label="CNPJ do Ente:"
-              ></v-text-field>
-            </v-form>
-          </v-col>
 
-
-          <v-col
-            cols="12"
-            sm="4"
-          >
-          <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+         <v-row>
             <v-btn class='btn-custom' variant="tonal" p-10>PESQUISAR</v-btn>
-            
-          </v-row>
-          <v-row>
-            <v-btn class='btn-custom' variant="tonal" p-10>SALVAR</v-btn>
-          </v-row>
-         </v-col>
+        
+         </v-row>
+         <v-row>
+           <v-btn class='btn-custom' variant="tonal" p-10>SALVAR</v-btn>
         </v-row>
+      </v-col>
+    </v-row>
 </template>
 
 <script lang="ts">
@@ -46,8 +46,7 @@
                 model: '',
                 max: 40,
                 rules: [
-                  (v: string) => !!v || 'Campo obrigatório',
-                  (v: string) => v.length <= 10 || 'Máximo 10 caracteres',
+                    (v: string) => !!v || 'Campo obrigatório',
                 ],
             }
         },
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-
     <PesquisarEnteForm />
 </template>
 
 <script lang="ts">
-import EnteTable from '../components/tables/EnteTable.vue';
-import PesquisarEnteForm from '../components/forms/PesquisarEnteForm.vue';
+    import EnteTable from '../components/tables/EnteTable.vue';
+    import PesquisarEnteForm from '../components/forms/PesquisarEnteForm.vue';
+    
     export default defineComponent({
         name: 'index',
         components: {
